c132e93945a3863cf11dfb09dadd20e50c24bd6e
zeext <muhammadzhikri48@gmail.com>
2020-11-20 03:13:48 +0700

    Adding Fox and  Controller

---
+++ UAS GKA/Assets/Script/FoxController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoxController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    Animator anim;
+
+    private void Awake() {
+        anim = this.GetComponent<Animator>();
+
+    }
+
+    private void FixedUpdate() {
+        float h = Input.GetAxis("Horizontal") * speed;
+        h *= Time.fixedDeltaTime;
+        transform.Translate(h, 0, 0);
+        if (Input.GetMouseButton(0))
+        {
+            anim.SetBool("isRunning", true);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (this.GetComponent<Animator>().GetBool("isGrounded") == true)
+            {
+                anim.SetBool("isGrounded", false);
+            }
+            else
+            {
+                anim.SetBool("isGrounded", true);
+            }
+        }
+
+    }
+}
