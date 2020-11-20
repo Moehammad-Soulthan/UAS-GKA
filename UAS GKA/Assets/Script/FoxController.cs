c4e410affb36b4aee46a64019c95af6e21507f6d
Moehammad-Soulthan <55024786+Moehammad-Soulthan@users.noreply.github.com>
2020-11-20 19:22:16 +0700

    Adding Animation

--- UAS GKA/Assets/Script/FoxController.cs
+++ UAS GKA/Assets/Script/FoxController.cs
@@ -4,25 +4,21 @@ using UnityEngine;
 
 public class FoxController : MonoBehaviour
 {
-    public float speed = 5.0f;
+    public float speed = 10.0f;
     Animator anim;
 
     private void Awake() {
         anim = this.GetComponent<Animator>();
-
     }
 
     private void FixedUpdate() {
-        float h = Input.GetAxis("Horizontal") * speed;
-        h *= Time.fixedDeltaTime;
-        transform.Translate(h, 0, 0);
-        if (Input.GetMouseButton(0))
-        {
-            anim.SetBool("isRunning", true);
-        }
+        float horizontalAxis = Input.GetAxis("Horizontal") * speed;
+        horizontalAxis *= Time.fixedDeltaTime;
+        transform.Translate(horizontalAxis, 0, 0);
 
-        else if (Input.GetKeyDown(KeyCode.Space))
-        {
+        if (Input.GetMouseButton(0)) {
+            anim.SetBool("isRunning", true);
+        } else if (Input.GetKeyDown(KeyCode.Space)) {
             if (this.GetComponent<Animator>().GetBool("isGrounded") == true)
             {
                 anim.SetBool("isGrounded", false);
@@ -33,5 +29,16 @@ public class FoxController : MonoBehaviour
             }
         }
 
+        if(Input.GetKey(KeyCode.LeftArrow)) {
+            anim.SetBool("isTurnLeft", true);
+        } else {
+            anim.SetBool("isTurnLeft", false);
+        }
+
+        if(Input.GetKey(KeyCode.RightArrow)) {
+            anim.SetBool("isTurnRight", true);
+        } else {
+            anim.SetBool("isTurnRight", false);
+        }
     }
 }
