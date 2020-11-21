317ae875854955dfd462ee7771e5dd4c3bc8af47
zeext <muhammadzhikri48@gmail.com>
2020-11-21 12:46:31 +0700

    Update Controller
    Adding A and D keys for left-right movement

--- UAS GKA/Assets/Script/PlayerController.cs
+++ UAS GKA/Assets/Script/PlayerController.cs
@@ -13,15 +13,47 @@ public class PlayerController : MonoBehaviour
     private float verticalVelocity;
     private float speed = 15.0f;
     private int desiredLane = 1; // 0 = Left, 1 = Middle, 2 = Right
+    Animator anim;
+
+    private void Awake() {
+        anim = this.GetComponent<Animator>();
+    }
+
 
     private void Start() {
         controller = GetComponent<CharacterController>();
     }
 
+    private void FixedUpdate() {
+        
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if (this.GetComponent<Animator>().GetBool("isGrounded") == true)
+            {
+                anim.SetBool("isGrounded", false);
+            }
+            else
+            {
+                anim.SetBool("isGrounded", true);
+            }
+        }
+
+        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
+            anim.SetBool("isTurnLeft", true);
+        } else {
+            anim.SetBool("isTurnLeft", false);
+        }
+
+        if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
+            anim.SetBool("isTurnRight", true);
+        } else {
+            anim.SetBool("isTurnRight", false);
+        }
+    }
+
     private void Update() {
-        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
             MoveLane(false);
-        if(Input.GetKeyDown(KeyCode.RightArrow))
+        if(Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
             MoveLane(true);
 
         Vector3 targetPosition = transform.position.z * Vector3.forward;
