74f51b857eca6aea317528a14470ace789af66dc
zeext <muhammadzhikri48@gmail.com>
2020-11-21 13:18:24 +0700

    Destroy Instantiated object
    added Prefabs.cs to Prefabs object

---
+++ UAS GKA/Assets/Script/Prefabs.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prefabs : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+         StartCoroutine("fordestroy");
+    }
+
+    // Update is called once per frame
+    IEnumerator fordestroy(){
+        yield return new WaitForSeconds(5f);
+        Destroy(this.gameObject);
+    }
+}
