3f8628938162a07082c31c610ce61fbd2e9ddcc8
Moehammad-Soulthan <55024786+Moehammad-Soulthan@users.noreply.github.com>
2020-11-22 12:54:48 +0700

    Add Simple Pause & Quit Menu UI

---
+++ UAS GKA/Assets/Script/PauseMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject pauseBtn;
+
+    void Update()
+    {
+        if(GameIsPaused) {
+            Pause();
+        } else {
+            Resume();
+        }
+    }
+
+    public void Resume() {
+        pauseMenuUI.SetActive(false);
+        pauseBtn.SetActive(true);
+        Time.timeScale = 1f;
+        GameIsPaused = false; 
+    }
+
+    public void Pause() {
+        pauseMenuUI.SetActive(true);
+        pauseBtn.SetActive(false);
+        Time.timeScale = 0f;
+        GameIsPaused = true; 
+    }
+
+    public void Quit() {
+        GameIsPaused = false; 
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("StartMenu");
+    }
+}
