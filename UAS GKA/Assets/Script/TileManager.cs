1ac49e71810f64be9ff37c3fd4183092039c1db6
andrew-ale <59736838+andrew-ale@users.noreply.github.com>
2020-11-25 23:21:48 +0700

    enemy spawner

--- UAS GKA/Assets/Script/TileManager.cs
+++ UAS GKA/Assets/Script/TileManager.cs
@@ -6,11 +6,13 @@ public class TileManager : MonoBehaviour
 {
     public GameObject[] tilePrefabs;
 
+    private List<GameObject> activeTiles;
+
     private Transform playerTransform;
     private float spawnZ = 0.0f;
     private float tileLength = 22.5f;
     private int amnTilesOnScreen = 6;
-    
+    private int lastPrefabIndex = 0;
 
     public GameObject sky;
     public GameObject mountain;
@@ -19,6 +21,7 @@ public class TileManager : MonoBehaviour
 
     void Start()
     {
+        activeTiles = new List<GameObject>();
         sky = GameObject.Find("sky");
         mountain = GameObject.Find("Mountain");
         cloud = GameObject.Find("Cloud");
@@ -26,9 +29,11 @@ public class TileManager : MonoBehaviour
 
         for (int i = 0; i < amnTilesOnScreen; i++)
         {
-            SpawnTiles();
+            if (i < 2)
+                SpawnTiles(0);
+            else
+                SpawnTiles();
         }
-
     }
 
     void Update()
@@ -36,27 +41,44 @@ public class TileManager : MonoBehaviour
         if (playerTransform.position.z > (spawnZ - amnTilesOnScreen * tileLength))
         {
             SpawnTiles();
-
             
             transformPosition(sky);
             transformPosition(mountain);
             transformPosition(cloud);
-    
         }
-      
     }
 
     void SpawnTiles(int prefabIndex = -1)
     {
         GameObject go;
-        go = Instantiate(tilePrefabs[0]) as GameObject;
+        if (prefabIndex == -1)
+            go = Instantiate(tilePrefabs[RandomPrefabIndex()]) as GameObject;
+        else
+            go = Instantiate(tilePrefabs[prefabIndex]) as GameObject;
+
         go.transform.SetParent(transform);
         go.transform.position = Vector3.forward * spawnZ;
         spawnZ += tileLength;
+        activeTiles.Add(go);
     }
 
     void transformPosition(GameObject obj) {
         positionIncrement += 9.0f;
         obj.transform.position = new Vector3(transform.position.x, transform.position.x, transform.position.z + positionIncrement);
+    }
+
+    private int RandomPrefabIndex()
+    {
+        if (tilePrefabs.Length <= 1)
+            return 0;
+
+        int randomIndex = lastPrefabIndex;
+        while (randomIndex == lastPrefabIndex)
+        {
+            randomIndex = Random.Range(0, tilePrefabs.Length);
+        }
+
+        lastPrefabIndex = randomIndex;
+        return randomIndex;
     }
 }
