9038b5eca530b2e832ec053f6bbc0238375893ba
samuell90 <samuelgunawan54@gmail.com>
2020-11-29 21:20:30 +0700

    credit show

---
+++ UAS GKA/Assets/Script/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Canvas credit;
+    void Start()
+    {
+        credit.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void creditshow()
+    {
+        credit.enabled = !credit.enabled;
+    }
+}
