7b4491a9b078fbb584efd73b427293e45f1b7a0e
Moehammad-Soulthan <55024786+Moehammad-Soulthan@users.noreply.github.com>
2020-12-03 14:51:51 +0700

    Update HowToPlay UI

--- UAS GKA/Assets/Script/GameManager.cs
+++ UAS GKA/Assets/Script/GameManager.cs
@@ -7,18 +7,22 @@ public class GameManager : MonoBehaviour
 {
     public Canvas credit;
     public Canvas highscore;
+    public Canvas howToPlay;
     public Button play;
     public GameObject creditPanel;
     public GameObject highscorePanel;
+    public GameObject howToPlayPanel;
 
 
     void Start()
     {
         credit.enabled = false;
         highscore.enabled = false;
+        howToPlay.enabled = false;
         play.enabled = true;
         creditPanel.gameObject.SetActive (false);
         highscorePanel.gameObject.SetActive (false);
+        howToPlayPanel.gameObject.SetActive (false);
     }
 
     // Update is called once per frame
@@ -48,6 +52,18 @@ public class GameManager : MonoBehaviour
             highscorePanel.gameObject.SetActive (true);
         } else {
             highscorePanel.gameObject.SetActive (false);
+        } 
+    }
+
+    public void howToPlayshow()
+    {
+        howToPlay.enabled = !howToPlay.enabled;
+        play.enabled = !play.enabled;
+
+        if(howToPlay.enabled) {
+            howToPlayPanel.gameObject.SetActive (true);
+        } else {
+            howToPlayPanel.gameObject.SetActive (false);
         } 
     }
 
